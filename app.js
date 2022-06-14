e43c3fdd0dba11d552dea69bf03edc281e9e428b
KeatonBrink <keatonbrink@gmail.com>
2022-06-14 11:38:36 -0600

    api added

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+const API_URL = "https://cs2022-eight-ball.herokuapp.com"
+/*
+Fetch Methods (Restful Api):
+Get - Recieve something from server
+Post - Give something to the server
+Put - Replace entry with new entry
+    Patch - Convoluted
+Delete 
+
+Example:
+fetch(URL + "/resource", {
+    method: "GET"
+})
+*/
+
 var app = new Vue ({
     el: "#app",
     data: {
         message: "",
+        messageColor: "",
+        answerReady: true,
         prompt: "Ask a question",
-        messageBank: [
-            "Yes",
-            "No",
-            "Maybe",
-            "Ask Again Tomorrow",
-            "Probably",
-            "Probably Not"
-        ],
+        // messageBank: [
+        //     "Yes",
+        //     "No",
+        //     "Maybe",
+        //     "Ask Again Tomorrow",
+        //     "Probably",
+        //     "Probably Not"
+        // ],
         //Question asked by user
         question: ""
     },
@@ -18,9 +35,26 @@ var app = new Vue ({
         askQuestion: function() {
             if (this.isValidQuestion()){
             //Not necessary as it is data this.question;
-            let nextIndex = Math.floor(Math.random()*this.messageBank.length);
-            let nextResponse = this.messageBank[nextIndex];
-            this.message = nextResponse;
+            // let nextIndex = Math.floor(Math.random()*this.messageBank.length);
+            // let nextResponse = this.messageBank[nextIndex];
+            //this.message = nextResponse;
+            this.answerReady = false;
+            this.message = "";
+            this.messageColor = "rgba(255, 255, 255, 0)";
+            setTimeout(() => {
+                fetch(API_URL + "/questions").then((response) => {
+                    response.json().then((data) => {
+                        console.log(data);
+                        this.message = data.answer;
+                        this.messageColor = data.color;
+                        setInterval(() => {
+                            this.answerReady = true;
+                        }, 1000);
+                        //This forces vue to update html
+                        //this.$forceUpdate();
+                    });
+                });
+            }, 2000);
             this.prompt = "Ask another question";
             this.question = "";
             }
@@ -28,6 +62,7 @@ var app = new Vue ({
         },
         isValidQuestion: function() {
             return this.question[this.question.length-1] == "?";
+    
         }
     }
 })
